9f7806d1dc0cd63386c2872326a6b10497207cfa
t-ashcraft <termisdash@gmail.com>
2025-04-29 10:52:57 -0400

    add page switching to top buttons

---
+++ src/home.js
@@ -0,0 +1,58 @@
+
+
+export function loadHome(content) {
+    content.innerHTML = 
+        `<div id="name-section">
+            <div id="name">Thomas</div>
+            <div id="name2">Ashcraft</div>
+            <div id="ticker"></div>
+        </div>
+
+        <div id="centerer">
+            <div id="about-me" class="section">
+                <h2 class="section-header">About Me</h2>
+                <p class="section-text">Hi! I'm <b>Thomas</b>, and I'm studying <b>computer science, math,</b> and <b>finance</b> at Purdue. Here you can learn about the projects I'm working on, find links to my socials, and explore my career development.</p>
+            </div>
+            <div id="section1" class="section">
+                <h2 class="section-header">Web Development</h2>
+                <div class="section-text-img">
+                    <p class="text-side">I've been making websites like this one over on <b>Github</b>. Click the link to check it out.</p>
+                    <div class="image-side">
+                        <button id="website-button">Check it out</button> 
+                    </div>
+                </div>
+            </div>
+            <div id="section2" class="section">
+                <h2 class="section-header">Game Development</h2>
+                <div class="section-text-img">
+                    <div class="image-side">
+                        <button id="gamedev-button">See More</button> 
+                    </div>
+                    <p class="text-side">I also make games in my free time, click <b>"See More"</b> to learn more about them.</p>
+                </div>
+            </div>
+            <div id="section3" class="section">
+                <h2 class="section-header">Career/Achievements</h2>
+                <div class="section-text-img">
+                    <p class="text-side">Want to learn more about my professional development? Click here.</p>
+                    <div class="image-side">
+                        <button id="career-button">Learn More</button> 
+                    </div>
+                </div>
+                
+            </div>
+            <div id="section4" class="section">
+                <h2 class="section-header">Where to Find Me</h2>
+                
+                <div class="section-text-img">
+                    <div class="image-side">
+                        <button id="socials-button">Contact Me</button> 
+                    </div>
+                    <p class="text-side">Need to contact me? Click the button to find out where.</p>
+
+                </div>
+            </div>
+        </div>`;
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,40 +1,87 @@
 import './style.css';
 
+import {loadHome} from "./home";
+
+const content = document.getElementById("content");
+
+const homeBtn = document.querySelector("#home");
+const websiteBtn = document.querySelector("#websites");
+const gamedevBtn = document.querySelector("#game-dev");
+const careerBtn = document.querySelector("#career");
+const socialBtn = document.querySelector("#social");
+
+function clearContent() {
+    content.innerHTML = "";
+}
+
+
+// comment out while working on other pages
+loadHome(content);
+
+
 
 const titles = ["game developer", "software designer", "problem solver", "eagle scout", "boilermaker", "data science researcher"];
-const ticker = document.getElementById("ticker");
+let ticker = document.querySelector("#ticker");
+
 
 let titleIndex = 0;
 let charIndex = 0;
 let typing = true;
+let mostRecentId;
 
 function typeTicker() {
     const currentTitle = titles[titleIndex];
-
+    if (mostRecentId !== undefined) {
+        clearTimeout(mostRecentId);
+    }
     if (typing) {
         if (charIndex < currentTitle.length) {
         ticker.textContent += currentTitle.charAt(charIndex);
         charIndex++;
         // delay per key
-        setTimeout(typeTicker, 100);
+        mostRecentId = setTimeout(typeTicker, 100);
         } else {
         typing = false;
         // delay per pause
-        setTimeout(typeTicker, 1500);
+        mostRecentId = setTimeout(typeTicker, 1500);
         }
     } else {
         if (charIndex > 0) {
         ticker.textContent = currentTitle.substring(0, charIndex - 1);
         charIndex--;
         // delay for backspacing
-        setTimeout(typeTicker, 50);
+        mostRecentId = setTimeout(typeTicker, 50);
         } else {
         typing = true;
         titleIndex = (titleIndex + 1) % titles.length;
         // delay before writing next
-        setTimeout(typeTicker, 300);
+        mostRecentId = setTimeout(typeTicker, 300);
         }
     }
-  }
+}
 
-  typeTicker();
+typeTicker(ticker);
+
+homeBtn.addEventListener("click", function() {
+    clearContent();
+    loadHome(content);
+    ticker = document.querySelector("#ticker");
+    typeTicker();
+    
+});
+
+websiteBtn.addEventListener("click", function() {
+    clearContent();
+});
+
+gamedevBtn.addEventListener("click", function() {
+    clearContent();
+});
+
+careerBtn.addEventListener("click", function() {
+    clearContent();
+});
+
+socialBtn.addEventListener("click", function() {
+    clearContent();
+});
